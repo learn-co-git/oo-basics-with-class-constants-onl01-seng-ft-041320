10036d7d24cd42007e0298f8a9d7c30a577894a0
Neil Empey <argus.two.2@gmail.com>
2020-04-20 15:31:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  Genres = []
+  Genres = ["thiller", "science fiction", ]
   def initialize(title)
     @title = title
   end
